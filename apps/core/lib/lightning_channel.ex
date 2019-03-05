4eedae497982ff686ed067d0bbb1f1b5ed149296
Grant West <grantwest2@gmail.com>
2019-03-05 11:15:08 -0600

    calc commitment working

--- apps/core/lib/lightning_channel.ex
+++ apps/core/lib/lightning_channel.ex
@@ -1,4 +1,5 @@
 defmodule Volta.Core.LightningChannel do
+  alias Volta.KeyUtils
   alias Volta.LightningMsg.OpenChannelMsg
   alias Volta.LightningMsg.AcceptChannelMsg
   alias Volta.LightningMsg.FundingCreatedMsg
@@ -19,14 +20,6 @@ defmodule Volta.Core.LightningChannel do
       :to_self_delay,
       :max_accepted_htlcs,
       :funding_pubkey,
-
-      :remote_dust_limit_satoshis_range,
-      :remote_max_htlc_value_in_flight_range,
-      :remote_channel_reserve_satoshis_range,
-      :remote_htlc_minimum_msat_range,
-      :remote_minimum_depth_range,
-      :remote_to_self_delay_range,
-      :remote_max_accepted_htlcs_range,
     ]
   end
 
@@ -40,17 +33,6 @@ defmodule Volta.Core.LightningChannel do
       :to_self_delay,
       :max_accepted_htlcs,
       :funding_pubkey,
-
-      :allowable_chain_hashes,
-      :remote_funding_satoshis_range,
-      :remote_push_msat_range,
-      :remote_dust_limit_satoshis_range,
-      :remote_max_htlc_value_in_flight_range,
-      :remote_channel_reserve_satoshis_range,
-      :remote_htlc_minimum_msat_range,
-      :remote_feerate_per_kw_range,
-      :remote_to_self_delay_range,
-      :remote_max_accepted_htlcs_range,
     ]
   end
 
@@ -72,40 +54,46 @@ defmodule Volta.Core.LightningChannel do
   end
 
   def open(%{state: :wait_open_ch} = ch) do
-    {:ok, %{ch | state: :wait_accept_ch}, open_msg(ch)}
+    to_send = open_msg(ch)
+    ch = %{ch | state: :wait_accept_ch}
+    {:ok, ch, to_send}
   end
 
   def receive(%{state: :wait_open_ch} = ch, %OpenChannelMsg{} = msg) do
-    {:ok, %{ch | state: :wait_funding_created}, accept_msg(ch)}
+    case validate(ch, msg) do
+      :valid -> 
+        ch = Map.put(ch, :channel_id, msg.temporary_channel_id)
+        ch = %{ch | state: :wait_funding_created}
+        {:ok, ch, accept_msg(ch)}
+
+      {:invalid, error_msg} -> {:reject, :invalid, error_msg}
+    end
   end
 
   def receive(%{state: :wait_accept_ch} = ch, %AcceptChannelMsg{} = msg) do
-    to_send = funding_created_msg(ch)
+    temp_ch_id = ch[:channel_id]
+    ch = %{ch | channel_id: 2}
+    to_send = funding_created_msg(ch, temp_ch_id)
     ch = %{ch | state: :wait_funding_signed}
     {:ok, ch, to_send}
   end 
 
   def receive(%{state: :wait_funding_created} = ch, %FundingCreatedMsg{} = msg) do
+    ch = %{ch | channel_id: msg.funding_txid}
     to_send = funding_signed_msg(ch)
     ch = %{ch | state: :wait_funding_locked}
     {:ok, ch, to_send}
   end
 
   def receive(%{state: :wait_funding_signed} = ch, %FundingSignedMsg{} = msg) do
-    funding_locked_msg = %FundingLockedMsg{
-      channel_id: 0,
-      next_per_commitment_point: 0,
-    }
+    to_send = funding_locked_msg(ch)
     ch = %{ch | state: :wait_funding_locked}
-    {:ok, ch, funding_locked_msg}
+    {:ok, ch, to_send}
   end
 
   def receive(%{state: :wait_funding_locked, role: :fundee} = ch, %FundingLockedMsg{} = msg) do
-    funding_locked_msg = %FundingLockedMsg{
-      channel_id: 1,
-      next_per_commitment_point: 1,
-    }
-    {:ok, ch, funding_locked_msg}
+    to_send = funding_locked_msg(ch)
+    {:ok, ch, to_send}
   end
 
   def receive(%{state: :wait_funding_locked, role: :funder} = ch, %FundingLockedMsg{} = msg) do
@@ -116,15 +104,15 @@ defmodule Volta.Core.LightningChannel do
     %OpenChannelMsg{
       chain_hash: opts.chain_hash,
       temporary_channel_id: ch_id,
-      funding_satoshis: 0,
-      push_msat: 0,
-      dust_limit_satoshis: 0,
-      max_htlc_value_in_flight_msat: 0,
-      channel_reserve_satoshis: 0,
-      htlc_minimum_msat: 0,
-      feerate_per_kw: 0,
-      to_self_delay: 0,
-      max_accepted_htlcs: 0,
+      funding_satoshis: opts.funding_satoshis,
+      push_msat: opts.push_msat,
+      dust_limit_satoshis: opts.dust_limit_satoshis,
+      max_htlc_value_in_flight_msat: opts.max_htlc_value_in_flight_msat,
+      channel_reserve_satoshis: opts.channel_reserve_satoshis,
+      htlc_minimum_msat: opts.htlc_minimum_msat,
+      feerate_per_kw: opts.feerate_per_kw,
+      to_self_delay: opts.to_self_delay,
+      max_accepted_htlcs: opts.max_accepted_htlcs,
       funding_pubkey: 0,
       revocation_basepoint: 0,
       payment_basepoint: 0,
@@ -156,10 +144,10 @@ defmodule Volta.Core.LightningChannel do
     }
   end
 
-  defp funding_created_msg(%{channel_id: ch_id}) do
+  defp funding_created_msg(%{channel_id: ch_id}, temp_ch_id) do
     %FundingCreatedMsg{
-      temporary_channel_id: ch_id,
-      funding_txid: 0,
+      temporary_channel_id: temp_ch_id,
+      funding_txid: ch_id,
       funding_output_index: 0,
       signature: 0,
     }
@@ -171,5 +159,50 @@ defmodule Volta.Core.LightningChannel do
       signature: 0,
     }
   end
+
+  defp funding_locked_msg(ch) do
+    %FundingLockedMsg{
+      channel_id: ch[:channel_id],
+      next_per_commitment_point: 0,
+    }
+  end
+
+  defp validate(ch, %OpenChannelMsg{} = msg) do
+    cond do 
+      msg.push_msat > 1000 * msg.funding_satoshis ->
+        {:invalid, "push (#{msg.push_msat} msat) cannot be greater than funding (#{msg.funding_satoshis} sat)"}
+
+      msg.to_self_delay > 26298 ->
+        {:invalid, "to_self_delay (#{msg.to_self_delay} blocks) is too large"}
+      
+      msg.max_accepted_htlcs > 483 ->
+        {:invalid, "max_accepted_htlcs (#{msg.max_accepted_htlcs}) cannot be greater than 483"}
+
+      msg.dust_limit_satoshis > msg.channel_reserve_satoshis ->
+        {:invalid, "dust limit (#{msg.dust_limit_satoshis} sat) cannot be greater than reserve (#{msg.channel_reserve_satoshis} sat)"}
+
+      !KeyUtils.valid_public_key?(msg.funding_pubkey) ->
+        {:invalid, "funding_pubkey (#{inspect(msg.funding_pubkey)}) is not a valid public key"}
+
+      !KeyUtils.valid_public_key?(msg.revocation_basepoint) ->
+        {:invalid, "revocation_basepoint (#{inspect(msg.revocation_basepoint)}) is not a valid public key"}
+
+      !KeyUtils.valid_public_key?(msg.payment_basepoint) ->
+        {:invalid, "payment_basepoint (#{inspect(msg.payment_basepoint)}) is not a valid public key"}
+
+      !KeyUtils.valid_public_key?(msg.delayed_payment_basepoint) ->
+        {:invalid, "delayed_payment_basepoint (#{inspect(msg.delayed_payment_basepoint)}) is not a valid public key"}
+
+      !KeyUtils.valid_public_key?(msg.htlc_basepoint) ->
+        {:invalid, "htlc_basepoint (#{inspect(msg.htlc_basepoint)}) is not a valid public key"}
+
+      !KeyUtils.valid_public_key?(msg.first_per_commitment_point) ->
+        {:invalid, "first_per_commitment_point (#{inspect(msg.first_per_commitment_point)}) is not a valid public key"}
+
+      true -> :valid
+    end
+  end
+
+
 
 end
--- apps/core/test/lightning_channel_test.exs
+++ apps/core/test/lightning_channel_test.exs
@@ -1,5 +1,6 @@
 defmodule Volta.Core.LightningChannelTest do
   use ExUnit.Case, async: true
+  alias Volta.KeyUtils
   alias Volta.Core.LightningChannel
   alias Volta.Core.LightningChannel.OpenChannelOpts
   alias Volta.Core.LightningChannel.AcceptChannelOpts
@@ -9,67 +10,50 @@ defmodule Volta.Core.LightningChannelTest do
   alias Volta.LightningMsg.FundingSignedMsg
   alias Volta.LightningMsg.FundingLockedMsg
 
-  @data %{
-    scenario_a: %{
+  defp scenario_a() do 
+    %{
       open_ch_opts: %OpenChannelOpts{
         chain_hash: 1234,
         temporary_channel_id: 1,
-        funding_satoshis: 0,
-        push_msat: 0,
-        dust_limit_satoshis: 0,
-        max_htlc_value_in_flight_msat: 0,
-        channel_reserve_satoshis: 0,
-        htlc_minimum_msat: 0,
-        feerate_per_kw: 0,
-        to_self_delay: 0,
-        max_accepted_htlcs: 0,
-        funding_pubkey: 0,
-        remote_dust_limit_satoshis_range: 0,
-        remote_max_htlc_value_in_flight_range: 0,
-        remote_channel_reserve_satoshis_range: 0,
-        remote_htlc_minimum_msat_range: 0,
-        remote_minimum_depth_range: 0,
-        remote_to_self_delay_range: 0,
-        remote_max_accepted_htlcs_range: 0,
+        funding_satoshis: 3_000_000,
+        push_msat: 4000,
+        dust_limit_satoshis: 5000,
+        max_htlc_value_in_flight_msat: 600_000_000,
+        channel_reserve_satoshis: 7000,
+        htlc_minimum_msat: 8000,
+        feerate_per_kw: 10,
+        to_self_delay: 11,
+        max_accepted_htlcs: 12,
+        funding_pubkey: "034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa" |> pubkey(),
       },
       accept_ch_opts: %AcceptChannelOpts{
-        dust_limit_satoshis: 0,
-        max_htlc_value_in_flight_msat: 0,
-        channel_reserve_satoshis: 0,
-        htlc_minimum_msat: 0,
-        minimum_depth: 0,
-        to_self_delay: 0,
-        max_accepted_htlcs: 0,
-        funding_pubkey: 0,
-        allowable_chain_hashes: 0,
-        remote_funding_satoshis_range: 0,
-        remote_push_msat_range: 0,
-        remote_dust_limit_satoshis_range: 0,
-        remote_max_htlc_value_in_flight_range: 0,
-        remote_channel_reserve_satoshis_range: 0,
-        remote_htlc_minimum_msat_range: 0,
-        remote_feerate_per_kw_range: 0,
-        remote_to_self_delay_range: 0,
-        remote_max_accepted_htlcs_range: 0,
+        dust_limit_satoshis: 500,
+        max_htlc_value_in_flight_msat: 60_000_000,
+        channel_reserve_satoshis: 700,
+        htlc_minimum_msat: 800,
+        minimum_depth: 13,
+        to_self_delay: 14,
+        max_accepted_htlcs: 15,
+        funding_pubkey: "034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa" |> pubkey(),
       },
       open_msg: %OpenChannelMsg{
         chain_hash: 1234,
         temporary_channel_id: 1,
-        funding_satoshis: 0,
-        push_msat: 0,
-        dust_limit_satoshis: 0,
-        max_htlc_value_in_flight_msat: 0,
-        channel_reserve_satoshis: 0,
-        htlc_minimum_msat: 0,
-        feerate_per_kw: 0,
-        to_self_delay: 0,
-        max_accepted_htlcs: 0,
-        funding_pubkey: 0,
-        revocation_basepoint: 0,
-        payment_basepoint: 0,
-        delayed_payment_basepoint: 0,
-        htlc_basepoint: 0,
-        first_per_commitment_point: 0,
+        funding_satoshis: 3_000_000,
+        push_msat: 4000,
+        dust_limit_satoshis: 5000,
+        max_htlc_value_in_flight_msat: 600_000_000,
+        channel_reserve_satoshis: 7000,
+        htlc_minimum_msat: 8000,
+        feerate_per_kw: 10,
+        to_self_delay: 11,
+        max_accepted_htlcs: 12,
+        funding_pubkey: "034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa" |> pubkey(),
+        revocation_basepoint: "034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa" |> pubkey(),
+        payment_basepoint: "034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa" |> pubkey(),
+        delayed_payment_basepoint: "034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa" |> pubkey(),
+        htlc_basepoint: "034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa" |> pubkey(),
+        first_per_commitment_point: "034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa" |> pubkey(),
         channel_flags: 0,
         shutdown_scriptpubkey: 0,
       },
@@ -94,30 +78,30 @@ defmodule Volta.Core.LightningChannelTest do
 
       funding_created_msg: %FundingCreatedMsg{
         temporary_channel_id: 1,
-        funding_txid: 0,
+        funding_txid: 2,
         funding_output_index: 0,
         signature: 0,
       },
 
       funding_signed_msg: %FundingSignedMsg{
-        channel_id: 1,
+        channel_id: 2,
         signature: 0,
       },
 
       funding_locked_msg_a: %FundingLockedMsg{
-        channel_id: 1,
+        channel_id: 2,
         next_per_commitment_point: 0,
       },
 
       funding_locked_msg_b: %FundingLockedMsg{
-        channel_id: 1,
-        next_per_commitment_point: 1,
+        channel_id: 2,
+        next_per_commitment_point: 0,
       }
     }
-  }
+  end
 
   test "open channel as initiator/funder/A" do
-    data = @data[:scenario_a]
+    data = scenario_a()
 
     ch = LightningChannel.create(:funder, data[:open_ch_opts])
     {:ok, ch, to_send} = LightningChannel.open(ch)
@@ -130,7 +114,7 @@ defmodule Volta.Core.LightningChannelTest do
   end
 
   test "open channel as responder/fundee/B" do
-    data = @data[:scenario_a]
+    data = scenario_a()
 
     ch = LightningChannel.create(:fundee, data[:accept_ch_opts])
     {:ok, ch, to_send} = LightningChannel.receive(ch, data[:open_msg])
@@ -139,6 +123,83 @@ defmodule Volta.Core.LightningChannelTest do
     assert to_send == data[:funding_signed_msg]
     {:ok, ch, to_send} = LightningChannel.receive(ch, data[:funding_locked_msg_a])
     assert to_send == data[:funding_locked_msg_b]
-  end  
+  end
+
+  test "do not accept if funding < push" do
+    data = scenario_a()
+    open_msg = %{data[:open_msg] | funding_satoshis: 3_000_000, push_msat: 3_000_000_001}
+
+    ch = LightningChannel.create(:fundee, data[:accept_ch_opts])
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "push (3000000001 msat) cannot be greater than funding (3000000 sat)"
+  end
+
+  test "do not accept if to self delay is too large" do
+    data = scenario_a()
+    open_msg = %{data[:open_msg] | to_self_delay: 26299}
+
+    ch = LightningChannel.create(:fundee, data[:accept_ch_opts])
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "to_self_delay (26299 blocks) is too large"
+  end
+
+  test "do not accept if max_accepted_htlcs > 483" do
+    data = scenario_a()
+    open_msg = %{data[:open_msg] | max_accepted_htlcs: 484}
+
+    ch = LightningChannel.create(:fundee, data[:accept_ch_opts])
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "max_accepted_htlcs (484) cannot be greater than 483"
+  end
+
+  test "do not accept if dust limit > reserve" do
+    data = scenario_a()
+    open_msg = %{data[:open_msg] | dust_limit_satoshis: 7001, channel_reserve_satoshis: 7000}
+
+    ch = LightningChannel.create(:fundee, data[:accept_ch_opts])
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "dust limit (7001 sat) cannot be greater than reserve (7000 sat)"
+  end
+
+  test "do not accept if any of the pub keys or base points are not valid public keys" do
+    data = scenario_a()
+    ch = LightningChannel.create(:fundee, data[:accept_ch_opts])
+
+    open_msg = %{data[:open_msg] | funding_pubkey: <<0, 1, 2>>}
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "funding_pubkey (<<0, 1, 2>>) is not a valid public key"
+
+    open_msg = %{data[:open_msg] | revocation_basepoint: <<0, 1, 2>>}
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "revocation_basepoint (<<0, 1, 2>>) is not a valid public key"
+
+    open_msg = %{data[:open_msg] | payment_basepoint: <<0, 1, 2>>}
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "payment_basepoint (<<0, 1, 2>>) is not a valid public key"
+
+    open_msg = %{data[:open_msg] | delayed_payment_basepoint: <<0, 1, 2>>}
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "delayed_payment_basepoint (<<0, 1, 2>>) is not a valid public key"
+
+    open_msg = %{data[:open_msg] | htlc_basepoint: <<0, 1, 2>>}
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "htlc_basepoint (<<0, 1, 2>>) is not a valid public key"
+
+    open_msg = %{data[:open_msg] | first_per_commitment_point: <<0, 1, 2>>}
+    {:reject, :invalid, error_msg} = LightningChannel.receive(ch, open_msg)
+    assert error_msg == "first_per_commitment_point (<<0, 1, 2>>) is not a valid public key"
+  end
+
+
+  defp hex(nil), do: nil
+  defp hex(b) do
+    Base.encode16(b, case: :lower)
+  end
+
+  defp bin(h) do
+    Base.decode16!(h, case: :lower)
+  end
+
+  defp pubkey(k), do: k |> bin() |> KeyUtils.decompress()
 
 end
